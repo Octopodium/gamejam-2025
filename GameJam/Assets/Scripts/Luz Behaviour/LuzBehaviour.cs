7ddde41ecb0988e2c10f79b3c943716e76f67944
anaclaragl <ag.linhares2@gmail.com>
2025-08-01 20:30:09 -0300

    Iluminação e Evento de troca de cor

---
+++ GameJam/Assets/Scripts/Luz Behaviour/LuzBehaviour.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LuzBehaviour : MonoBehaviour{
+    public Color red, blue, green;
+    public Light outterLight;
+    public MeshRenderer outterCircle, innerCircle;
+
+    private float innerAlpha = 0.8f, outterAlpha = 0.0705f;
+
+    public UnityAction<Color> OnMudaCor;
+
+    public void Start(){
+        OnMudaCor += MudaCor;
+    }
+
+    public void MudaCor(Color color){
+        outterLight.color = color;
+
+        color.a = innerAlpha;
+        innerCircle.material.color = color;
+        color.a = outterAlpha;
+        outterCircle.material.color = color;
+    }
+}
