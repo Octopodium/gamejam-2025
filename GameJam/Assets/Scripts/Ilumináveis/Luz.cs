eeb553548f17730f26d4b58b6aed8223cde85d43
anaclaragl <ag.linhares2@gmail.com>
2025-08-02 02:32:05 -0300

    Iluminavel ta reagindo a luz

--- GameJam/Assets/Scripts/Ilumináveis/Luz.cs
+++ GameJam/Assets/Scripts/Ilumináveis/Luz.cs
@@ -1,6 +1,32 @@
 using UnityEngine;
 
-public class Luz : MonoBehaviour
-{
+public class Luz : MonoBehaviour{
+    float raioIluminacao = 3.9f;
+    public LayerMask ilumiavelLayer;
+    Collider[] collidersIluminaveis;
+    Iluminavel iluminavel;
+
+    public void Start(){
+        collidersIluminaveis = new Collider[8];
+    }
+    
+    public void FixedUpdate(){
+        ChecarIluminaveis();
+    }
+
+    public void ChecarIluminaveis(){
+        int iluminaveisQuant = Physics.OverlapSphereNonAlloc(transform.position, raioIluminacao, collidersIluminaveis, ilumiavelLayer);
+
+        for(int i = 0; i < iluminaveisQuant; i++){
+            iluminavel = collidersIluminaveis[i].GetComponent<Iluminavel>();
+            iluminavel.Reagir();
+
+        }
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, raioIluminacao);
+    }
 
 }
